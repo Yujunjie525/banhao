1b555afc83ee5b2b603785ff333e7e0b21c9a032
yujunjie <963527610@qq.com>
2026-09-09 17:19:49 +0800

    Import local projects: batch 83/102
    Import 439 files from the current local project snapshots.

---
+++ zuihoudeyingxiong/assets/mySdk/AdParam.ts
@@ -0,0 +1,11 @@
+export class AdParam {
+    public static tt_params = {
+        /************** 广告参数配置 ***************/
+        banner_id: "1klh52489699688a01",          //bannerid
+        rewarded_video_id0: "1b4sah6gq4s75lea8s",      //激励视频id
+        interstitial_id: "3op4rdd4fk64nfl3jv",        //插屏id
+    }
+
+    
+    
+}
